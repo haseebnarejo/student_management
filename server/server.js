c535fee8ac6cd1a1b067097c815fc6f570d04f96
haseebnarejo <muhammadhaseebnarejo>
2025-03-11 14:50:48 +0500

    frontend

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "",
+    database: "school_management"
+})
+
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM student";
+    db.query(sql, (err, result) => {
+        if(err) return res.json({Message: "Error inside server"});
+        return res.json(result);
+    })
+})
+
+app.listen(8081, () => {
+    console.log("listening");
+    
+})
