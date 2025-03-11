043a068ff9f3d9b4cf78737867ea6fbe3ddc783b
haseebnarejo <muhammadhaseebnarejo>
2025-03-11 17:36:31 +0500

    read

--- frontend/src/Read.jsx
+++ frontend/src/Read.jsx
@@ -1,18 +1,34 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Button } from 'bootstrap';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const Read = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const [student, setStudent] = useState([])
+    
+    
 
     useEffect(() => {
         axios.get('http://localhost:8081/read/' + id)
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
+            .then(res => {
+                console.log(res)
+                setStudent(res.data)
+            })
+            .catch(err => console.log(err))
     }, [])
-  return (
-    <div>Read</div>
-  )
+    return (
+        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+            <div className="w-50 bg-white rounded p-3">
+                    <h2>Student Detail</h2>
+                    <h2>{student[0].id}</h2>
+                    <h2>{student[0].name}</h2>
+                    <h2>{student[0].email}</h2>
+                    <Link to='/' className="btn btn-primary me-2">Back</Link>
+                    <button className="btn btn-info">Edit</button>
+            </div>
+        </div>
+    )
 }
 
 export default Read
